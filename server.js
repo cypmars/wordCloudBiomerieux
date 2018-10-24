789770d968765727e6d7d9f88a4ba3251ff667e7
cypconnet <cyprien.connet@capgemini.com>
2018-10-24 20:12:31 +0200

    13.0

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 
-const express = require('express');
-const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
